e87ba9af0852d649df8be177510309d9d7777846
wolfses50 <85653914+wolfses50@users.noreply.github.com>
2023-08-15 11:08:12 -0300

    feat: Mas requests

--- Backend/router.ts
+++ Backend/router.ts
@@ -4,5 +4,9 @@ import * as controllers from "./controllers";
 const router = Router();
 
 router.get("/user/:dni", controllers.getUser);
+router.get("/user:dni/posts", controllers.getUserPosts);
+router.get("/user/:dni/reviews", controllers.getPostReview);
+router.get("/user/:dni/home", controllers.getHome);
+router.get("/user/:dni/home/search", controllers.getPostsBySearch);
 
 export default router;
