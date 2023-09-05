872c7f4b8cb91587c8df62edf15dd9ecdb7d9f87
Ulises Wolfzun <wolfzunulises@gmail.com>
2023-09-05 09:20:12 -0300

    feat: caca

--- Frontend/Pagina-inicial/Pagina-inicial.js
+++ Frontend/Pagina-inicial/Pagina-inicial.js
@@ -1,5 +1,6 @@
 var slides = document.querySelectorAll('.slide');
 var btns = document.querySelectorAll('.btn');
+var slideId = button.dataset.postId;
 let currentSlide = 0;
 
 var manualNav = function(manual){
@@ -46,15 +47,9 @@ var repeat = function(activeClass){
             }
 
             slides[i].classList.add('active');
-            fetch(`http://localhost:3000/post/i`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: {}
-            })
-            btns[i].classList.add('active');
-            
+            slides[1].addEventListener('click', ()=>{
+                const url = "http://localhost:3000/post/${post.id}";
+            });
             repeater(); 
         }, 5000);
     }
