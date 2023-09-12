fa1de49492e29b912c9c49e89ca996b04e374cdc
Fedezafran1 <48111347@est.ort.edu.ar>
2023-09-12 14:16:18 -0300

    Update algoritmo.py

--- IA/algoritmo.py
+++ IA/algoritmo.py
@@ -1,16 +1,3 @@
 #import pandas as pd
 #import sqlite3
 #import psycopg2
-
-#con_sqlite = sqlite3.connect(DATABASE_URL='mysql://jj1j7erszbp1hxkgk2ot:pscale_pw_UIUpWr5B9Ph3IvBIZToAMCOMCLNLvVqqd94rWStk8Ot@aws.connect.psdb.cloud/outller?sslaccept=strict')
-
-
-
-
-
-import mysql.connector
-
-conexion = mysql.connector.connect(user = 'u70yokvfncmgbx40aqk7', password = 'pscale_pw_gejJapzORKA05XTJoeEmVvh2ZVq70MaNO0XOTRMuLAK',
-                                   host = 'aws.connect.psdb.cloud',
-                                   database = ''
-                                   port = '')
