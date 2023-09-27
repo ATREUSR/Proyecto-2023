9741913e77762b5e0bfda20368a5abf65d877583
Fedezafran1 <48111347@est.ort.edu.ar>
2023-09-27 09:30:11 -0300

    Update conectar.py

--- IA/practica sist recomm/algoritmo desde bd/conectar.py
+++ IA/practica sist recomm/algoritmo desde bd/conectar.py
@@ -21,34 +21,3 @@ if version:
     print(version)
 else:
     print('Not connected.')
-
-import csv
-
-data = [
-    (3, 47871184, 'Ulises', 'Wolfzun', 'wolfzunulises@gmail.com', '$2b$10$yC7WleIt5JdGgGMkVVRqneQ9ZEGLCx9AhLRoIwwlF5R5YTFQnCIk2', 0, '+54 911 64632684', "O' Higgins 3861"),
-    (4, 47394146, 'Agustin', 'Colinas', 'Agustincolinas6@gmail.com', '$2b$10$rVIe8ZsePjrbyCLEi7zymOfUv80WU7plCe6ju0GG7SgX5QHz/a69q', 0, '+54 911 64632684', "O' Higgins 3861"),
-    (7, 473146, 'Agustin', 'Colinas', 'Agustinconas6@gmail.com', '$2b$10$wvQkV1f/z8RE6kx4NIlKTeM/W6wtv3.fJXKje/Sq3AVzUwCqkfrCG', 0, '+54 911 64632684', "O' Higgins 3861")
-]
-
-# Nombre del archivo CSV
-csv_filename = "datos_user.csv"
-
-# Crear y escribir en el archivo CSV
-with open(csv_filename, 'w', newline='') as csvfile:
-    csv_writer = csv.writer(csvfile)
-    
-    # Escribir el encabezado del CSV
-    csv_writer.writerow(["Nombre", "Apellido", "Dirección", "Número de Teléfono", "DNI"])
-    
-    # Escribir los datos de la lista en el CSV
-    for row in data:
-        csv_writer.writerow([row[2], row[3], row[8], row[7], row[1]])
-
-print(f"Los datos se han guardado en '{csv_filename}' como un archivo CSV.")
-
-
-import pandas as pd
-
-user_df = pd.read_csv("datos_user.csv", encoding="latin1")
-
-print(user_df)
