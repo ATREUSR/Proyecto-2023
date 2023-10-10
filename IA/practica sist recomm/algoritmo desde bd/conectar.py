07acf7350a9a82370eb2dc1083d0da050bb09c57
Fedezafran1 <48111347@est.ort.edu.ar>
2023-10-10 13:36:40 -0300

    .

--- IA/practica sist recomm/algoritmo desde bd/conectar.py
+++ IA/practica sist recomm/algoritmo desde bd/conectar.py
@@ -41,27 +41,27 @@ user = pd.read_csv("user.csv")
 print(user)
 print(user.shape)
 
-cursor.execute("SELECT * FROM Post")
+cursor.execute("SELECT * FROM Review")
 version2 = cursor.fetchall()
 if version2:
     print(version2)
 else:
     print('Not connected.')
 
-result2 = [[entry[1], entry[2], entry[4], entry[5], entry[6]] for entry in version2]
+result2 = [[entry[0], entry[1], entry[2], entry[3], entry[4]] for entry in version2]
 
-csv_file_path = 'post.csv'
+csv_file_path = 'Review.csv'
 
 # Write the extracted information to a CSV file
 with open(csv_file_path, mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['id', 'titulo', 'descripcion', 'defectos', 'precio'])  # Write header
+    writer.writerow(['0', '1', '2', '3', '4'])  # Write header
     writer.writerows(result2)
 
-post = pd.read_csv("post.csv")
-print(post)
+Review = pd.read_csv("Review.csv")
+print(Review)
 
-descripcion_columna = post['precio']
+descripcion_columna = Review['4']
 
 # Imprimir los primeros 5 valores de la columna 'descripcion'
 print(descripcion_columna.head())
