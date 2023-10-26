bb92d34f19757f0e5f4c6102251f69b31473a802
Fedezafran1 <48111347@est.ort.edu.ar>
2023-10-26 13:43:29 -0300

    Update conectar.py

--- IA/practica sist recomm/algoritmo desde bd/conectar.py
+++ IA/practica sist recomm/algoritmo desde bd/conectar.py
@@ -13,7 +13,7 @@ conn = sql.connect(host = db_host,
                    password = db_passwd,
                    db = db_name)
 cursor = conn.cursor()
-cursor.execute("SELECT user_id,title,defects,description,price FROM Post")
+cursor.execute("SELECT Liked")
 version = cursor.fetchall()
 
 
@@ -26,60 +26,60 @@ else:
 
 #print(result)
 
-import csv
+# import csv
 
-csv_file_path = 'post.csv'
-
-# Write the extracted information to a CSV file
-with open(csv_file_path, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['id', 'title', 'defectos', 'description', 'Price'])  # Write header
-    writer.writerows(version)
-
-import pandas as pd
-post = pd.read_csv("post.csv")
-post = post.dropna()
-print(post)
-user = pd.read-csv("user.csv")
-user = user.dropna()
-print(user)
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-# Vectorización de descripciones de productos
-tfidf_vectorizer = TfidfVectorizer()
-producto_descripciones = tfidf_vectorizer.fit_transform(post['description'])
-
-from sklearn.preprocessing import LabelEncoder
-
-label_encoder = LabelEncoder()
-#user['UsuarioID'] = label_encoder.fit_transform(user['UsuarioID'])
-
-
-
-# print(user.shape)
-
-# cursor.execute("SELECT * FROM Review")
-# version2 = cursor.fetchall()
-# if version2:
-#     print(version2)
-# else:
-#     print('Not connected.')
-
-# result2 = [[entry[0], entry[1], entry[2], entry[3], entry[4]] for entry in version2]
-
-# csv_file_path = 'Review.csv'
+# csv_file_path = 'post.csv'
 
 # # Write the extracted information to a CSV file
 # with open(csv_file_path, mode='w', newline='') as file:
 #     writer = csv.writer(file)
-#     writer.writerow(['0', '1', '2', '3', '4'])  # Write header
-#     writer.writerows(result2)
+#     writer.writerow(['id', 'title', 'defectos', 'description', 'Price'])  # Write header
+#     writer.writerows(version)
 
-# Review = pd.read_csv("Review.csv")
-# print(Review)
+# import pandas as pd
+# post = pd.read_csv("post.csv")
+# post = post.dropna()
+# print(post)
+# user = pd.read-csv("user.csv")
+# user = user.dropna()
+# print(user)
+# from sklearn.feature_extraction.text import TfidfVectorizer
 
-# descripcion_columna = Review['4']
+# # Vectorización de descripciones de productos
+# tfidf_vectorizer = TfidfVectorizer()
+# producto_descripciones = tfidf_vectorizer.fit_transform(post['description'])
 
-# # Imprimir los primeros 5 valores de la columna 'descripcion'
-# print(descripcion_columna.head())
+# from sklearn.preprocessing import LabelEncoder
+
+# label_encoder = LabelEncoder()
+# #user['UsuarioID'] = label_encoder.fit_transform(user['UsuarioID'])
+
+
+
+# # print(user.shape)
+
+# # cursor.execute("SELECT * FROM Review")
+# # version2 = cursor.fetchall()
+# # if version2:
+# #     print(version2)
+# # else:
+# #     print('Not connected.')
+
+# # result2 = [[entry[0], entry[1], entry[2], entry[3], entry[4]] for entry in version2]
+
+# # csv_file_path = 'Review.csv'
+
+# # # Write the extracted information to a CSV file
+# # with open(csv_file_path, mode='w', newline='') as file:
+# #     writer = csv.writer(file)
+# #     writer.writerow(['0', '1', '2', '3', '4'])  # Write header
+# #     writer.writerows(result2)
+
+# # Review = pd.read_csv("Review.csv")
+# # print(Review)
+
+# # descripcion_columna = Review['4']
+
+# # # Imprimir los primeros 5 valores de la columna 'descripcion'
+# # print(descripcion_columna.head())
 
