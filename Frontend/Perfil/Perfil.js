1b31334e05b2223e0713d0d877a3393d3438804e
ATREUSR <romanljsa@gmail.com>
2023-11-13 00:50:04 -0300

    Feat: starting with the profile and settings page

---
+++ Frontend/Perfil/Perfil.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const settingOptions = document.querySelectorAll(".setting-option");
+    const settingContents = document.querySelectorAll(".setting-content");
+
+    settingOptions[0].classList.add("active");
+
+    settingContents[0].classList.add("active");
+
+    settingOptions.forEach((option, index) => {
+        option.addEventListener("click", () => {
+            // Oculta todos los contenidos de ajustes
+            settingContents.forEach(content => content.classList.remove("active"));
+
+            // Resalta la opción seleccionada
+            settingOptions.forEach(opt => opt.classList.remove("active"));
+            option.classList.add("active");
+
+            // Muestra solo el contenido correspondiente a la opción seleccionada
+            settingContents[index].classList.add("active");
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
