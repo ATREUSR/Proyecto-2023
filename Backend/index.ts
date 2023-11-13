862a554b46e23a75d25cd5e93cf6ed7cb61ccd0e
Ulises Wolfzun <wolfzunulises@gmail.com>
2023-11-13 15:35:01 -0300

    Bueno ehm, anda

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import router from "./router";
-import cors from "cors";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+import cors  from "cors";
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -23,12 +23,23 @@ async function testCloudinaryConnection() {
 
 testCloudinaryConnection();
 
+const allowedOrigins = [  'http://localhost:3000', 'http://localhost:8080', 'http://localhost:0080', 'http://localhost:80', 'http://localhost' ];
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(router);
 app.use(cors({
-  origin: ["http://localhost:80", "http://localhost:3000", "http://localhost:443", "http://localhost:8080"], credentials: true
-}))
+  origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
+    //console.log(origin);
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+app.use(router);
+
 const PORT: Number = 3000;
 app.listen(PORT, () => console.log("Alive on localhost: " + PORT));
