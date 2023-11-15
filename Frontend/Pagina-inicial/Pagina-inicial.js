9e74035d073e92eb32ad5438c1dcaf932ccd9091
ATREUSR <romanljsa@gmail.com>
2023-11-15 16:04:20 -0300

    feat: continue with functionaly with id

--- Frontend/Pagina-inicial/Pagina-inicial.js
+++ Frontend/Pagina-inicial/Pagina-inicial.js
@@ -27,7 +27,7 @@ const priceElements = document.querySelectorAll('.item-price[data-price]');
 });
 */
 
-itemContainers.forEach((itemContainer, index) => {
+itemContainer.forEach((itemContainer, index) => {
     itemContainer.addEventListener("click", () => {
         // Obtén el ID del producto (puedes usar el índice actual como ejemplo)
         const productId = index + 1; // Puedes ajustar esto según tus necesidades
@@ -38,6 +38,62 @@ itemContainers.forEach((itemContainer, index) => {
 });
 
 
+
+//en el html tieve que haber una variable como las de precio que va a guardar un id. El atributo de este id va a ser asignado aca. Ylos valkores de nombre y demas van a ser los que encuentre en ese id. Osea que el valor de los atributos sera el del valor que tenga el id de esos atributos. Va a habr un for que revise cada item preguntado por su id y rellenando los valors con sus id values.
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const specificPriceElement = document.querySelector('.item-price[data-price="5000"]');
+    const specificNameElement = document.querySelector('.item-name[data-name="Iphone 14"]');
+    const specificDescElement = document.querySelector('.item-info[data-desc="Iphone 14 con pixeles muertos"]');
+
+    priceElements.forEach(function (priceElement) {
+        var unitPrice = parseFloat(priceElement.getAttribute('data-price'));
+        priceElement.querySelector('h3').innerText = '$' + unitPrice.toFixed(0);
+    });  
+
+    nameElements.forEach(function (nameElement){
+        var uniteName = nameElement.getAttribute('data-name');
+        nameElement.querySelector('h3').innerText = uniteName;
+    });
+
+    descElements.forEach(function (descElement){
+        var uniteDesc = descElement.getAttribute('data-desc');
+        descElement.querySelector('p').innerText = uniteDesc;
+    });
+
+    if (specificPriceElement) {
+        // Cambia el valor del data-price
+        specificPriceElement.setAttribute('data-price', '6000');
+
+        // Obtiene el nuevo valor y lo muestra en el elemento
+        var newUnitPrice = parseFloat(specificPriceElement.getAttribute('data-price'));
+        specificPriceElement.querySelector('h3').innerText = '$' + newUnitPrice.toFixed(0);
+    }
+
+    if (specificNameElement) {
+        // Cambia el valor del data-price
+        specificNameElement.setAttribute('data-name', 'samsung');
+
+        // Obtiene el nuevo valor y lo muestra en el elemento
+        var newUnitName = specificNameElement.getAttribute('data-name')
+        specificNameElement.querySelector('h3').innerText = newUnitName;
+    }
+
+    if (specificDescElement) {
+        // Cambia el valor del data-price
+        specificDescElement.setAttribute('data-desc', 'S21 con pantalla rota');
+
+        // Obtiene el nuevo valor y lo muestra en el elemento
+        var newUnitDesc = specificDescElement.getAttribute('data-desc')
+        specificDescElement.querySelector('p').innerText = newUnitDesc;
+    }
+});
+
+function changeInfo(item)
+{
+    item = document.querySelectorAll('item-container');
+}
 
 var manualNav = function(manual){
     slides.forEach((slide) => {
