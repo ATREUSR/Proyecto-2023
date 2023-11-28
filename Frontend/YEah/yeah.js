605830985c3ed3c3a35dbf7e07b2608e5b79d39b
ATREUSR <romanljsa@gmail.com>
2023-11-27 23:50:42 -0300

    odio todo

--- Frontend/YEah/yeah.js
+++ Frontend/YEah/yeah.js
@@ -1,6 +1,15 @@
+let title = document.getElementById("title_id");
+let userId = document.getElementById("user_id");
+let price = document.getElementById("price_id");
+let desc = document.getElementById("desc_id");
+let defects = document.getElementById("defects_id");
+let hasDefects = document.getElementById("hasdefects_id");
+let imgType = document.getElementById("imgtype_id");
+let imgFile = document.getElementById("imgfile_id");
 let submitBtn = document.getElementById("submit_id");
 
 submitBtn.addEventListener("click", () => {
+    console.log(title.value);
     fetch('http://localhost:3000/post/', {
       method: 'POST',
       headers: {
